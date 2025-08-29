de6843b87c2c6e0df4e8d9668175f6ef0683cf06
developmentexpert121 <developmentexpert121@gmail.com>
2025-08-29 19:52:11 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0", // so dev server listens on all IPs, useful for AWS/ngrok
-    port: 5173,      // optional: specify the port
-    allowedHosts: ["36ba5cade7d0.ngrok-free.app", "localhost"], // whitelist your hostname(s)
+    allowedHosts: ["36ba5cade7d0.ngrok-free.app"],
   },
 });

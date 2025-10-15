5c4b8b967c08bd0ac6a02deb67e5047342f0f478
Developmentexpert121 <developmentexpert121@gmail.com>
2025-10-15 14:26:38 +0530

    Header changes 15-10-2025

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,18 @@ const Header = () => {
 
   return (
     <header className="flex items-center p-4 bg-white shadow-md w-full">
-      <div className="container max-w-[1120px] mx-auto flex justify-between">
+      <div className="container max-w-[1120px] mx-auto flex justify-between items-center">
+        {/* Logo on the left */}
         <div>
-          <div className="">
-            <img
-              src="/logo1.png"
-              alt="Logo"
-              className="w-[300px] h-[80px] object-contain"
-            />
-          </div>
+          <img
+            src="/logo1.png"
+            alt="Logo"
+            className="w-[300px] h-[80px] object-contain"
+          />
+        </div>
 
+        {/* Button on the right */}
+        <div>
           <button
             onClick={handleBack}
             className="cursor-pointer flex items-center gap-2 text-[#d56b61] font-semibold border border-[#d56b61] px-10 py-2 rounded-full transition-all duration-300 hover:bg-[#d56b61] hover:text-white hover:shadow-lg hover:scale-105"

e7bcd7ad3d9197189470611ab239d112110c27f3
Developmentexpert121 <developmentexpert121@gmail.com>
2025-10-15 11:53:57 +0530

    updated code

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import logo from "../../public/logo1.png";
+
 const Header = () => {
   const handleBack = () => {
     window.location.href = "https://www.sniffout.io/";
@@ -8,12 +10,16 @@ const Header = () => {
       <div className="container max-w-[1120px] mx-auto flex justify-between">
         <div>
           <div className="">
-            <img src="."></img>
+            <img
+              src="/logo1.png"
+              alt="Logo"
+              className="w-[300px] h-[80px] object-contain"
+            />
           </div>
 
           <button
             onClick={handleBack}
-            className="flex items-center gap-2 text-[#d56b61] font-semibold border border-[#d56b61] px-4 py-2 rounded-full transition-all duration-300 hover:bg-[#d56b61] hover:text-white hover:shadow-lg hover:scale-105"
+            className="cursor-pointer flex items-center gap-2 text-[#d56b61] font-semibold border border-[#d56b61] px-10 py-2 rounded-full transition-all duration-300 hover:bg-[#d56b61] hover:text-white hover:shadow-lg hover:scale-105"
           >
             ← Back
           </button>

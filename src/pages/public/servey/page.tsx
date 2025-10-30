29f031f2033f606d0bf95ab835c61d807b73201b
Developmentexpert121 <developmentexpert121@gmail.com>
2025-10-30 18:26:07 +0530

    updated code offers 30-10-2025

--- src/pages/public/servey/page.tsx
+++ src/pages/public/servey/page.tsx
@@ -1,6 +1,6 @@
 // src/pages/public/PublicSurveyPage.tsx
 import React, { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import {
   Star,
   Check,
